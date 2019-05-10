a7a5252c8687640d7d062fd2256979ac8a18b530
Default Profile <lise.p.poirier@gmail.com>
2019-05-10 09:31:01 +0200

    Retirer OnClick du js

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -22,9 +22,15 @@ var tabTictactoe = {
             whoWins();
         }
     };
-    document.getElementById('c0').addEventListener("click", function () {
-        play("c0");
-    });
+    document.getElementById('c0').addEventListener("click", function () { play("c0"); });
+    document.getElementById('c1').addEventListener("click", function () { play("c1"); });
+    document.getElementById('c2').addEventListener("click", function () { play("c2"); });
+    document.getElementById('c3').addEventListener("click", function () { play("c3"); });
+    document.getElementById('c4').addEventListener("click", function () { play("c4"); });
+    document.getElementById('c5').addEventListener("click", function () { play("c5"); });
+    document.getElementById('c6').addEventListener("click", function () { play("c6"); });
+    document.getElementById('c7').addEventListener("click", function () { play("c7"); });
+    document.getElementById('c8').addEventListener("click", function () { play("c8"); });
 })();
 
 function play(elt) {
